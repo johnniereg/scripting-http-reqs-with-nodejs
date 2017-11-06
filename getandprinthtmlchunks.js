2cbebfdfd5976e53610d2072210c8ddd0d9f6968
Johnnie Regalado <johnniereg@gmail.com>
2017-11-06 19:15:50 +0000

    Cleaned up code and added some comments.

--- getandprinthtmlchunks.js
+++ getandprinthtmlchunks.js
@@ -12,17 +12,12 @@ function getAndPrintHTMLChunks () {
     // set encoding of data received to UTF-8
     response.setEncoding('utf8');
 
+    // print out data as it is received, put new line between
     response.on('data', function(data) {
 
       console.log(data + '\n');
-
-
     });
-
-
   });
-
 }
-
 
 getAndPrintHTMLChunks();
