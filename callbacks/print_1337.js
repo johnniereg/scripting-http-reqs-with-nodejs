cf362fec1543f5c4efa9565631a54b5c7de19ec3
Johnnie Regalado <johnniereg@gmail.com>
2017-11-07 04:15:56 +0000

    Started building the 1337 speak function, got it working for single characters. Still need to tackle multi-character. Current build probably won't work.

--- callbacks/print_1337.js
+++ callbacks/print_1337.js
@@ -7,10 +7,31 @@ var requestOptions = {
   path: '/http-examples/step6/1337.html'
 };
 
+function switchLetters (letter) {
+  switch(letter) {
+    case 'a': return '4';
+    case 'e': return '3';
+    case 'l': return '1';
+    case 'o': return '0';
+    case 's': return '5';
+    case 't': return '7';
+    default: return letter;
+  }
+}
+
 function print1337 (html) {
 
-  /* Write your code here! */
+  var stringedArray = html.split("");
 
-}
+  var leetString = "";
+
+  stringedArray.forEach(function(letter) {
+
+    leetString += switchLetters(letter);
+
+  });
+
+  console.log(leetString);
+};
 
 getHTML(requestOptions, print1337);
